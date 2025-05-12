cd888de8498f64843fc489ee586393494e6bb616
manuelgtzb <ricardoemmanuel.gutierrezbetancourt.gen23@cbtis105.edu.mx>
2025-05-12 10:08:19 -0600

    12/05/25

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicity = null);
 
 var app = builder.Build();
 
