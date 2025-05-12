45d4f1a5395e7e0d6018c5535be686fd2b2177d5
manuelgtzb <ricardoemmanuel.gutierrezbetancourt.gen23@cbtis105.edu.mx>
2025-05-12 09:07:10 -0600

    Proyecto Inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OrganizadorDeVuelos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
